cd2c11d7df27ad0a6b84043a5fc20328d857bbb8
Brian Otieno <bryomajor@gmail.com>
2020-01-18 18:00:59 +0300

    install Django.

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Desktop/core/projects/django/portfolio/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
