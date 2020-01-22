e1272b1e8950d7a9906353096c1e354613c257ce
Brian Otieno <bryomajor@gmail.com>
2020-01-22 21:49:58 +0300

    create project model.

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    pass
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    link = models.URLField(max_length=250)
+    image = models.ImageField(upload_to='project_images')
+
+    def __str__(self):
+        return self.name
